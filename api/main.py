e72577c550b96ec976137bd43245293f4592768c
Remco Goyvaerts <remco.goyvaerts@acagroup.be>
2023-12-12 11:29:56 +0100

    Add basic crud

--- api/main.py
+++ api/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 
+from .db import models
+from .db.database import engine
 from .routers import sessions
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
--- api/routers/sessions.py
+++ api/routers/sessions.py
@@ -1,8 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from ..db import crud
+from ..dependencies import get_db
 
 router = APIRouter(prefix="/sessions", tags=["sessions"])
 
 
 @router.get("/")
-async def get_sessions():
-    return []
+async def read_sessions(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    return crud.get_sessions(db, skip=skip, limit=limit)
