a154948ae52031cd5305da3812e8f172b6b0d007
RemcoGoy <remco.goy@hotmail.com>
2023-12-13 20:37:11 +0100

    Add pydantic models

--- api/db/schemas.py
+++ api/db/schemas.py
@@ -3,16 +3,80 @@ import uuid
 from pydantic import BaseModel
 
 
+class IdBase:
+    id: uuid.UUID
+
+
+class LapBase(BaseModel):
+    pass
+
+
+class LapCreate(LapBase):
+    pass
+
+
+class Lap(IdBase, LapBase):
+    class Config:
+        orm_mode = True
+
+
 class SessionBase(BaseModel):
-    laps: int
+    pass
 
 
 class SessionCreate(SessionBase):
     pass
 
 
-class Session(SessionBase):
+class Session(IdBase, SessionBase):
     id: uuid.UUID
+    track_id: uuid.UUID
+    car_id: uuid.UUID
+    laps: list[Lap]
 
+    class Config:
+        orm_mode = True
+
+
+class TrackBase(BaseModel):
+    pass
+
+
+class TrackCreate(TrackBase):
+    pass
+
+
+class Track(IdBase, TrackBase):
+    game_id: uuid.UUID
+
+    class Config:
+        orm_mode = True
+
+
+class GameBase(BaseModel):
+    name: str
+
+
+class GameCreate(GameBase):
+    pass
+
+
+class Game(IdBase, GameBase):
+    tracks: list[Track]
+
+    class Config:
+        orm_mode = True
+
+
+class CarBase(BaseModel):
+    name: str
+    type: str
+
+
+class CarCreate(CarBase):
+    pass
+
+
+class Car(IdBase, CarBase):
     class Config:
         orm_mode = True
