46226ad2554c6e51f359a0d188cf8e0a7d9a83a5
RemcoGoy <remco.goy@hotmail.com>
2023-12-14 16:48:13 +0100

    Create game

--- api/db/crud.py
+++ api/db/crud.py
@@ -107,4 +107,12 @@ def get_games(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Game).offset(skip).limit(limit).all()
 
 
+def create_game(db: Session, game: schemas.GameCreate):
+    db_game = models.Game(name=game.name)
+    db.add(db_game)
+    db.commit()
+    db.refresh(db_game)
+    return db_game
+
+
 # endregion
